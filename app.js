a0aceb93f196c593a403366ab5fb666deb4447f5
BrunoAnjos <brunosilveiradosanjos@gmail.com>
2021-05-09 11:33:55 -0300

    Inserting tour and selecting by ID

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const fs = require('fs');
 const express = require('express')
 const app = express();
+// important
+app.use(express.json());
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+const fileLocation = `${__dirname}/dev-data/data/tours-simple.json`;
+const tours = JSON.parse(fs.readFileSync(fileLocation));
 
 app.get('/api/v1/tours', (req, res) => {
     res.status(200).json({
@@ -12,6 +15,44 @@ app.get('/api/v1/tours', (req, res) => {
             tours: tours
         }
     })
+})
+
+app.get('/api/v1/tours/:id', (req, res) => {
+    const id = req.params.id * 1
+    if (id > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        })
+    }
+    const tour = tours.find(el => el.id === id)
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tours: tour
+        }
+    })
+})
+
+app.post('/api/v1/tours', (req, res) => {
+    console.log(req.body);
+    const newId = tours[tours.length - 1] + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+
+    tours.push(newTour);
+
+    fs.writeFile(
+        fileLocation,
+        JSON.stringify(tours),
+        err => {
+            res.status(201).json({
+                status: 'success',
+                data: {
+                    tour: newTour
+                }
+            });
+        });
+
 })
 
 const port = 3000;
