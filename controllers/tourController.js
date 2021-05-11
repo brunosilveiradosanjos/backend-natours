4fcfbb72be08c5a8697499d8b4e9940ddcb97245
BrunoAnjos <brunosilveiradosanjos@gmail.com>
2021-05-10 23:11:24 -0300

    GETting from cloud database

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,12 +3,45 @@ const Tour = require('../models/tourModel');
 // middlewere
 
 // tours
-exports.getAllTours = (req, res) => {
-    res.status(200).json({})
+exports.getAllTours = async (req, res) => {
+
+    try {
+
+        const tours = await Tour.find();
+        res.status(200).json({
+            status: 'success',
+            result: tours.length,
+            data: {
+                tours
+            }
+        });
+
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: `Something went wrong: ${err}`
+        });
+    }
 };
 
-exports.getOneTour = (req, res) => {
-    res.status(200).json({})
+exports.getOneTour = async (req, res) => {
+
+    try {
+
+        const tour = await Tour.findById(req.params.id);
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tour
+            }
+        });
+
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: `Something went wrong: ${err}`
+        });
+    }
 };
 
 exports.createTour = async (req, res) => {
