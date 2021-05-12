cabd8e15610e5660919e5b0642ba48796417041f
BrunoAnjos <brunosilveiradosanjos@gmail.com>
2021-05-11 21:42:59 -0300

    Making the API Better: Advanced Filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -6,14 +6,24 @@ const Tour = require('../models/tourModel');
 exports.getAllTours = async (req, res) => {
     try {
         // We need a hard copy, if we just set const queryObj = req.query then if we delete something from queyrObj we would also delete id from .query object "..." is a shortcut for destructuring
+
         // BUILD QUERY
+
+        // 1)Filtering
         const queryObj = { ...req.query };
         const excludeFields = ['page', 'sort', 'limit', 'fields'];
         excludeFields.forEach(el => delete queryObj[el]);
-        console.log(excludeFields, queryObj);
+        // console.log(excludeFields, queryObj);
 
-        const query = Tour.find(queryObj);
+        // 2)Advanced Filtering
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
 
+        console.log(req.query);
+        console.log(JSON.parse(queryStr));
+
+
+        const query = Tour.find(JSON.parse(queryStr));
         // EXECUTE QUERY
         const tours = await query;
 
