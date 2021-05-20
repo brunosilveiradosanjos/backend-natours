ba78abf6fa81965375f85369ad161d5b708ab5dc
BrunoAnjos <brunosilveiradosanjos@gmail.com>
2021-05-19 23:43:39 -0300

    Data Validation: Build-in Validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,25 +5,33 @@ const tourSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'A Tour must have a name'],
-        unique: true
+        unique: true,
+        trim: true,
+        maxlength: [40, 'A tour name must have less or equal then 40 to characters'],
+        minlength: [10, 'A tour name must have more or equal then 10 to characters']
     },
     slug: String,
     duration: {
         type: Number,
         required: [true, 'A tour must have a duration']
     },
-
     maxGroupSize: {
         type: Number,
         required: [true, 'A tour must have a group size']
     },
     difficulty: {
         type: String,
-        required: [true, 'A tour must have a difficulty']
+        required: [true, 'A tour must have a difficulty'],
+        enum: {
+            values: ['easy', 'medium', 'difficult'],
+            message: 'Difficulty is either: easy, medium, difficult'
+        }
     },
     ratingsAverage: {
         type: Number,
-        default: 4.5
+        default: 4.5,
+        min: [1, 'Rating must be above 1.0'],
+        max: [5, 'Rating must be above 5.0']
     },
     ratingsQuantity: {
         type: Number,
