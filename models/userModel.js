e8e3b8b1d1106952800bfaf9d6b5867eb43b2c4c
BrunoAnjos <brunosilveiradosanjos@gmail.com>
2021-05-20 21:55:44 -0300

    Modeling users

---
+++ models/userModel.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const usetSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'You must provide a user name'],
+        trim: true,
+        maxlength: [40, 'A tour name must have less or equal then 40 to characters'],
+        minlength: [10, 'A tour name must have more or equal then 10 to characters']
+    },
+    email: {
+        type: String,
+        required: [true, 'You must provide a user email'],
+        trim: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'Please provide a valid email']
+    },
+    photo: {
+        type: String,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: [true, 'You must provide a user password'],
+        minlength: 8
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, 'Please confirm your password']
+    }
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
+
