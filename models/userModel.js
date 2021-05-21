023673adfb0a4d8c0b65f96a73b4979bab32ada9
BrunoAnjos <brunosilveiradosanjos@gmail.com>
2021-05-20 23:37:00 -0300

    Creating new Users

--- models/userModel.js
+++ models/userModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-const usetSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'You must provide a user name'],
@@ -12,6 +12,7 @@ const usetSchema = new mongoose.Schema({
     email: {
         type: String,
         required: [true, 'You must provide a user email'],
+        unique: true,
         trim: true,
         lowercase: true,
         validate: [validator.isEmail, 'Please provide a valid email']
