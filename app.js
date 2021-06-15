f57f0fd3f881e51f4105d0671bcfc7ad12ce7ab4
BrunoAnjos <brunosilveiradosanjos@gmail.com>
2021-06-15 00:48:59 -0300

    Data Sanitization

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 const tourRouter = require('./routes/tourRoutes')
 const userRouter = require('./routes/userRoutes')
 
@@ -27,6 +29,10 @@ const limiter = rateLimit({
 app.use('/api', limiter);
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize()); //"email": {"$gt":""},
+// Data sanitization against XSS
+app.use(xss()); // convert html simbols
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
 // Test middleware
