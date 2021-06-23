744ab09636e6f11df460247bcca8f560485dce3b
BrunoAnjos <brunosilveiradosanjos@gmail.com>
2021-06-23 00:17:13 -0300

    Populating Reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -34,6 +34,17 @@ reviewSchema = new mongoose.Schema({
     }
 );
 
+reviewSchema.pre(/^find/, function (next) {
+    this.populate({
+        path: 'tour',
+        select: 'name'
+    }).populate({
+        path: 'user',
+        select: 'name photo'
+    });
+    next();
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
