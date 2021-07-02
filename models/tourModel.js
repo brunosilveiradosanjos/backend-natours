0512d9026e4e098559fa6ea5ac4c49664a585911
BrunoAnjos <brunosilveiradosanjos@gmail.com>
2021-07-02 00:57:55 -0300

    Geospatial Aggregation: Calculating Distances

--- models/tourModel.js
+++ models/tourModel.js
@@ -166,12 +166,12 @@ tourSchema.pre(/^find/, function (next) {
 })
 
 // AGGREGATION MIDDLEWARE
-tourSchema.pre('aggregate', function (next) {
-    // adding to all aggregate middleware to auto exclude secretTour with this filter
-    this.pipeline().unshift({ $match: { secretTour: { $ne: true } } })
-    // console.log(this.pipeline());
-    next();
-})
+// tourSchema.pre('aggregate', function (next) {
+//     // adding to all aggregate middleware to auto exclude secretTour with this filter
+//     this.pipeline().unshift({ $match: { secretTour: { $ne: true } } })
+//     // console.log(this.pipeline());
+//     next();
+// })
 
 tourSchema.post(/^find/, function (docs, next) {
     console.log(`Query took ${Date.now() - this.start} milliseconds!`);
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -29,6 +29,10 @@ router
     .get(tourController.getToursWithin);
 
 router
+    .route('/distances/:latlng/unit/:unit')
+    .get(tourController.getDistances);
+
+router
     .route('/')
     .get(tourController.getAllTours)
     // .post(tourController.checkBody, tourController.createTour)
