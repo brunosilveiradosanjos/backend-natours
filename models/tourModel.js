0f31e08776cafc5371d576cb56aa53c79725eb04
BrunoAnjos <brunosilveiradosanjos@gmail.com>
2021-07-01 23:45:30 -0300

    Geospatial Queries: Finding Tours Within Radius

--- models/tourModel.js
+++ models/tourModel.js
@@ -123,6 +123,7 @@ const tourSchema = new mongoose.Schema({
 
 tourSchema.index({ price: 1, ratingsAverage: -1 });
 tourSchema.index({ slug: 1 })
+tourSchema.index({ startLocation: '2dsphere' });
 
 // VIRTUAL PROPERTIES return duration in weeks
 tourSchema.virtual('durationWeeks').get(function () {
